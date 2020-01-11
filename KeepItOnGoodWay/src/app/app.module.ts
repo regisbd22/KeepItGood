d7c1922b8e3dc33ba7d6a9e60276d13d880577f9
regis <regis.bougaud@gmail.com>
2020-01-11 20:29:36 +0100

    install bootstrap/jquery/popper and generate components

--- KeepItOnGoodWay/src/app/app.module.ts
+++ KeepItOnGoodWay/src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './Auth/sign-in/sign-in.component';
+import { SignUpComponent } from './Auth/sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { MainGestionComponent } from './Gestion/main-gestion/main-gestion.component';
+import { AccountingComponent } from './accounting/accounting.component';
+
+const roads = [
+  { path : 'auth/signin', component: SignInComponent },
+  { path : 'auth/signup',  component: SignUpComponent },
+  { path : '',  component: HomeComponent },
+  { path : 'gestion/mainPage',  component: MainGestionComponent },
+  { path : 'accounting',  component: AccountingComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent,
+    MainGestionComponent,
+    AccountingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(roads)
   ],
   providers: [],
   bootstrap: [AppComponent]
